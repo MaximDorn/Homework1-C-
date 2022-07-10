26c5727ddaccd37a067fea223e75e8f30ec4edda
Max Dorn <maximilian.sd.84@gmail.com>
2022-07-10 22:30:18 +0300

    Results after seminar

---
+++ SolutionTask1/Program.cs
@@ -0,0 +1,20 @@
+﻿string? InputLineOne = Console.ReadLine();
+string? InputLineTwo = Console.ReadLine();
+
+if (InputLineOne != null && InputLineTwo != null)
+{
+    int InputNumberOne = int.Parse(InputLineOne);
+    int InputNumberTwo = int.Parse(InputLineTwo);
+    
+    if (InputNumberTwo == Math.Sqrt(InputNumberOne))
+    {
+        Console.WriteLine("Yes");
+    }
+    
+    else
+    {
+        Console.WriteLine("No");
+    }
+
+
+}
---
+++ SolutionTask5/Program.cs
@@ -0,0 +1,20 @@
+﻿string? InputLine = Console.ReadLine();
+
+if (InputLine != null)
+{
+    int inputNumber = int.Parse(InputLine);
+    
+    int startNumber = 1;
+
+string lineOutput = "0";
+
+while (startNumber <= inputNumber)
+{
+lineOutput = startNumber *(-1) + ", "+ lineOutput + ", " + startNumber;
+startNumber++;
+}
+
+// lineOutput = lineOutput + inputNumber;
+
+    Console.WriteLine(lineOutput);
+}
