cf18e7b91230ca8adf54fc0dbdd2dbc70053ce3e
Max Dorn <maximilian.sd.84@gmail.com>
2022-07-11 00:44:59 +0300

    Add SolutionTask4

---
+++ SolutionTask4/Program.cs
@@ -0,0 +1,25 @@
+﻿string? inputLineA = Console.ReadLine();
+string? inputLineB = Console.ReadLine();
+string? inputLineC = Console.ReadLine();
+
+if (inputLineA != null && inputLineB != null && inputLineC != null)
+{
+    int inputNumberA = int.Parse(inputLineA);
+    int inputNumberB = int.Parse(inputLineB);
+    int inputNumberC = int.Parse(inputLineC);
+    
+    int Max = inputNumberA;
+
+    if (inputNumberB > Max)
+    {
+        Max = inputNumberB;
+    }
+    
+    if (inputNumberC > Max)
+    {
+        Max = inputNumberC;
+    }
+
+Console.WriteLine(Max);
+
+}
