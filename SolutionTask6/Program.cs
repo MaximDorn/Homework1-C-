e739a772861ccd44f60261c5561bcb1dce80cef3
Max Dorn <maximilian.sd.84@gmail.com>
2022-07-11 01:00:40 +0300

    Add SolutionTask6

---
+++ SolutionTask6/Program.cs
@@ -0,0 +1,22 @@
+﻿string? inputLine = Console.ReadLine();
+
+if (inputLine != null)
+{
+    int inputNumber = int.Parse(inputLine);
+    
+    int Test = inputNumber%2;
+
+    if (Test == 0)
+    {
+        Console.WriteLine("Число является чётным");
+    }
+
+    else
+    {
+        Console.WriteLine("Число является нечётным");
+    }
+
+
+
+    
+}
