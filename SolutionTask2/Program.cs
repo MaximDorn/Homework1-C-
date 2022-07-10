5407ba41bacfb3ed480adf1bcb97f5463d587362
Max Dorn <maximilian.sd.84@gmail.com>
2022-07-11 00:20:41 +0300

    Add SolutionTask2

---
+++ SolutionTask2/Program.cs
@@ -0,0 +1,19 @@
+﻿string? inputLineA = Console.ReadLine();
+string? inputLineB = Console.ReadLine();
+
+if (inputLineA != null && inputLineB != null)
+{
+    int inputNumberA = int.Parse(inputLineA);
+    int inputNumberB = int.Parse(inputLineB);
+    
+    if (inputNumberA > inputNumberB)
+    {
+        Console.WriteLine(inputNumberA + ">" + inputNumberB + "  -->" + "  Max="+ inputNumberA +", " + "Min="+ inputNumberB);
+    }
+    
+    else
+    {
+        Console.WriteLine(inputNumberB + ">" + inputNumberA + "  -->" + "  Max="+ inputNumberB +", " + "Min="+ inputNumberA);
+    }
+    
+}
