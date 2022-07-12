4b6be73fb4bf3970a1356422f1b2d7022c509baf
Max Dorn <maximilian.sd.84@gmail.com>
2022-07-12 23:30:41 +0300

    Add Example010 Method Find

---
+++ Example010_MethodFind/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = {3, 423, 53, 4, 32, 1, 2, 3, 543, 4, 21};
+
+int n = array.Length;
+int find = 4;
+
+ int index = 0;
+ 
+ while (index < n)
+ {
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+ }
