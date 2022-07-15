2ba15846e537b81ab2d8198e72fba722ac5bbdc3
Max Dorn <maximilian.sd.84@gmail.com>
2022-07-16 02:27:39 +0300

    Add SolutionTask16

---
+++ SolutionTask16/Program.cs
@@ -0,0 +1,21 @@
+﻿// Решение от Никиты
+Console.WriteLine("Введите первое число:");
+string? inputFirst = Console.ReadLine();
+Console.WriteLine("Введите второе число:");
+string? inputSecond = Console.ReadLine();
+
+if ((inputFirst != null) && (inputSecond != null))
+{
+    int inputNumberFirst = int.Parse(inputFirst);
+    int inputNumberSecond = int.Parse(inputSecond);
+    int a = inputNumberFirst * inputNumberFirst;
+    int b = inputNumberSecond * inputNumberSecond;
+    if ((a == inputNumberSecond) || (b == inputNumberFirst))
+    {
+        Console.WriteLine("Да!");
+    }
+    else
+    {
+        Console.WriteLine("Нет!");
+    }
+}
