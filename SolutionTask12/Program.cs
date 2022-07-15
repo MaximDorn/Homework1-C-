24d8b84cb42deba0b444af7531d656be8e055019
Max Dorn <maximilian.sd.84@gmail.com>
2022-07-16 01:57:07 +0300

    Add SolutionTask12

---
+++ SolutionTask12/Program.cs
@@ -0,0 +1,38 @@
+﻿// Решение Николая
+// string? inputLineOne = Console.ReadLine();
+// string? inputLineTwo = Console.ReadLine();
+
+// if (inputLineOne != null && inputLineTwo != null)
+// {
+//     int inputNumberOne = int.Parse(inputLineOne);
+//     int inputNumberTwo = int.Parse(inputLineTwo);
+
+//     int div = inputNumberTwo % inputNumberOne;
+
+//     if (div == 0)
+//     {
+//         Console.WriteLine("yes");
+//     }
+//     else
+//     {
+//         Console.WriteLine(div);
+//     }
+
+// }
+
+//Решение Артёма
+Console.Write("Enter first number: ");
+string? firstLine = Console.ReadLine();
+Console.Write("Enter second number: ");
+string? secondLine = Console.ReadLine();
+int first = int.Parse(firstLine);
+int second = int.Parse(secondLine);
+
+if (second % first == 0)
+{
+    Console.WriteLine("Кратное");
+}
+else
+{
+    Console.WriteLine("Не кратное, " + second % first);
+}
