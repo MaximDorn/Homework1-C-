7faa82a0442bfe21d787cccc95e3b5a15cc5bfab
Max Dorn <maximilian.sd.84@gmail.com>
2022-07-17 01:00:04 +0300

    Add SolutionTask13

---
+++ SolutionTask13/Program.cs
@@ -0,0 +1,17 @@
+﻿// Программа, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.WriteLine("Пожалуйста, введите число: ");
+string? inputNumber = Console.ReadLine();
+
+if (inputNumber != null)
+{
+    int inputNumberOne = int.Parse(inputNumber);
+    Char[] array = inputNumber.ToCharArray(); // Преобразование введённой строки в массив методом "ToCharArray"
+    if (array.Length > 2) // Проверка наличия третьей цифры (длина массива > 2)
+    {
+        Console.WriteLine(array[2]);
+    }
+    else
+    {
+        Console.WriteLine("Третьей цифры нет");
+    }
+}
