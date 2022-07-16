73243f28aec424faa469b649a3d67f6a0c180084
Max Dorn <maximilian.sd.84@gmail.com>
2022-07-17 00:11:17 +0300

    Add SolutionTask10

--- SolutionTask10/Program.cs
+++ SolutionTask10/Program.cs
@@ -3,14 +3,14 @@ int number = new Random().Next(100, 1000);
 Console.Write("Number is: " + number);
 Console.WriteLine(", Second digit is:");
 
-    // Solution 1: сначала деление без остатка на 10, затем остаток от деления на 10
-    int numberSecondDigit = (number / 10) % 10;
-    Console.WriteLine("- solution 1: " + numberSecondDigit);
+// Solution 1: сначала деление без остатка на 10, затем остаток от деления на 10
+int numberSecondDigit = (number / 10) % 10;
+Console.WriteLine("- solution 1: " + numberSecondDigit);
 
-    // Solution 2: через перевод числа в строку
-    string numberAsString = number.ToString();
-    Console.WriteLine("- solution 2: " + numberAsString[1]);
+// Solution 2: через перевод числа в строку
+string numberAsString = number.ToString();
+Console.WriteLine("- solution 2: " + numberAsString[1]);
 
-    // Solution 3: через преобразование числа в массив
-    int[] array = {number/100, ((number/10)%10), number%10};
-    Console.WriteLine("- solution 3: " + array[1]);
+// Solution 3: через преобразование числа в массив
+int[] array = { number / 100, ((number / 10) % 10), number % 10 };
+Console.WriteLine("- solution 3: " + array[1]);
