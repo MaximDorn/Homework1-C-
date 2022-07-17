9b8ecd2d67d530be64d82d56d68966c18ca7d071
Max Dorn <maximilian.sd.84@gmail.com>
2022-07-17 14:02:06 +0300

    SolutionTask15 correction

--- SolutionTask13/Program.cs
+++ SolutionTask13/Program.cs
@@ -7,7 +7,7 @@ if (inputNumber != null)
     char[] array = inputNumber.ToCharArray(); // Преобразование введённой строки в массив методом "ToCharArray"
     if (array.Length > 2) // Проверка наличия третьей цифры (длина массива > 2)
     {
-        Console.WriteLine("Третья цифра числа: " + array[2]);
+        Console.WriteLine("Третья цифра числа: " + array[2]); // "array[2]", потому что отчёт в массиве ведётся с индекса "0"
     }
     else
     {
