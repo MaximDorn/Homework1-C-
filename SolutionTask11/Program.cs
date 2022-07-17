d736409b0e233e9864000dc7aa5b4fb8798cb24b
Max Dorn <maximilian.sd.84@gmail.com>
2022-07-17 14:07:36 +0300

    SolutionTask11 experiments

--- SolutionTask11/Program.cs
+++ SolutionTask11/Program.cs
@@ -8,26 +8,26 @@
 // Решение Кирилла
 void middleDigitCutter()
 {
-int number = new Random().Next(100, 1000);
-Console.Write("The random number is : ");
-Console.WriteLine(number);
-string stringNum = number.ToString();
-Console.Write("The modified num is : ");
-Console.Write(stringNum[0]);
-Console.WriteLine(stringNum[2]);
+    int number = new Random().Next(100, 1000);
+    Console.Write("The random number is : ");
+    Console.WriteLine(number);
+    string stringNum = number.ToString();
+    Console.Write("The modified num is : ");
+    Console.Write(stringNum[0]);
+    Console.WriteLine(stringNum[2]);
 }
 
 void middleDigitCutter2()
 {
-int number = new Random().Next(100, 1000);
-Console.Write("The random number is : ");
-Console.WriteLine(number);
+    int number = new Random().Next(100, 1000);
+    Console.Write("The random number is : ");
+    Console.WriteLine(number);
 
-int num1 = number / 100;
-int num2 = number % 10;
+    int num1 = number / 100;
+    int num2 = number % 10;
 
-Console.Write("The modified num is : ");
-Console.WriteLine(num1*10 + num2);
+    Console.Write("The modified num is : ");
+    Console.WriteLine(num1 * 10 + num2);
 }
 
 middleDigitCutter();
