0c20a615954b9bf75983616f3569e6502a5004b2
Max Dorn <maximilian.sd.84@gmail.com>
2022-07-19 23:30:59 +0300

    TaskSolution20

---
+++ SolutionTask20/Program.cs
@@ -0,0 +1,32 @@
+﻿int coordXA;
+int coordYA;
+int coordXB;
+int coordYB;
+double lengthAB;
+
+// Метод считывает координаты точек A и B
+void readDataOfPoint()
+{
+    Console.WriteLine("Введите координату X точки A");
+    coordXA = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Введите координату Y точки A");
+    coordYA = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Введите координату X точки B");
+    coordXB = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Введите координату Y точки B");
+    coordYB = int.Parse(Console.ReadLine());
+}
+
+// Вычисляет расстояние между точками A и B
+void calculateLengthAB()
+{
+    lengthAB = Math.Sqrt(Math.Pow((coordXA - coordXB), 2) + Math.Pow((coordYA - coordYB), 2));
+}
+
+readDataOfPoint();
+calculateLengthAB();
+
+Console.WriteLine(lengthAB);
