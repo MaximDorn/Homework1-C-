12de5b5389aedb4a10ac2a6f5e8bb941ab1e4d06
Max Dorn <maximilian.sd.84@gmail.com>
2022-07-21 14:10:16 +0300

    TaskSolution23 with table

--- SolutionTask23/Program.cs
+++ SolutionTask23/Program.cs
@@ -8,18 +8,28 @@ if (inputLineN != null)
     int numberN = int.Parse(inputLineN); // Парсим введённое число в int
 
     // Вводим необходимые переменные
-    string lineN = string.Empty;
-    string lineNNN = string.Empty;
-    int s = 1; 
+    int lineN = 0;
+    int lineNNN = 0;
 
+    int s = 1;
+
+    // Cоздаём шапку для таблицы 
+    Console.WriteLine(new string('-', 27));
+    Console.WriteLine("|{0,-12}|{1,-12}|", "Число", "Куб");
+    Console.WriteLine(new string('-', 27));
+
+    // Каждый цикл будет прописывать 2 значения: число и число возведённое в куб.
     while (s <= numberN)
     {
-        lineN = lineN + s + " ";
-        lineNNN = lineNNN + s * s * s + " "; // LineNNN будет накапливать кубы
-        s++;
-    }
+        lineN = s; // Числа без возведения в степень
+        Console.Write("| {0, -10} | ", lineN);
 
-    Console.WriteLine(lineN);
-    Console.WriteLine(lineNNN);
-    Console.WriteLine("Число\tКвадрат\tКуб");
+        lineNNN = s * s * s; // Возводим числа в куб
+        Console.Write("{0, -10} | ", lineNNN);
+
+        s++;
+
+        Console.WriteLine(); // Начать с новой строки
+        Console.WriteLine(new string('-', 27)); // Прорисовать линию и начать с новой строки
+    }
 }
